f27f823500b3d391ac7e0603a80ff1e682054a1e
sharfuddinzishan <sharfuddin.ahamed91@gmail.com>
2021-09-07 19:44:33 +0600

    [updated & Added] HTML DOM Element Added

---
+++ scripts/wallet.js
@@ -0,0 +1,10 @@
+const header = document.getElementById('header');
+const total = document.getElementById('total');
+const income = document.getElementById('income');
+const expenses = document.getElementById('expenses');
+const transactionType = document.getElementById('transactionType');
+const transactionNote = document.getElementById('transactionNote');
+const transactionAmount = document.getElementById('transactionAmount');
+const transactionSubmit = document.getElementById('transactionSubmit');
+const transactionHistory = document.getElementById('transactionHistory');
+
