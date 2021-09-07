b96a77cdda530f846eb9a447c8bb0bccc817a9f6
sharfuddinzishan <sharfuddin.ahamed91@gmail.com>
2021-09-07 19:57:50 +0600

    [Added] invokeTransaction functionality

--- scripts/wallet.js
+++ scripts/wallet.js
@@ -1,10 +1,18 @@
 const header = document.getElementById('header');
-const total = document.getElementById('total');
-const income = document.getElementById('income');
-const expenses = document.getElementById('expenses');
+const totalBalance = document.getElementById('totalBalance');
+const totalIncome = document.getElementById('totalIncome');
+const totalExpenses = document.getElementById('totalExpenses');
 const transactionType = document.getElementById('transactionType');
 const transactionNote = document.getElementById('transactionNote');
 const transactionAmount = document.getElementById('transactionAmount');
 const transactionSubmit = document.getElementById('transactionSubmit');
 const transactionHistory = document.getElementById('transactionHistory');
 
+transactionSubmit.addEventListener('click', invokeTransaction);
+
+function invokeTransaction() {
+    let type = transactionType.value;
+    let note = transactionNote.value;
+    let amount = transactionAmount.value;
+    console.log(type, note, amount)
+}
